600249ee29b0ae44607b6d7c548a4a39aadfc50a
okamura123 <119551754+okamura123@users.noreply.github.com>
2023-01-23 16:00:07 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter } from "react-router-dom";
+import { PrimaryButton } from "./components/atoms/button/PrimatyButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
+import "./styles.css";
+
+const user = {
+  name: "2",
+  image:
+    "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  email: "123456@example.com",
+  phone: "090-1234-5678",
+  company: {
+    name: "あいう株式会社"
+  },
+  website: "https://google.com"
+};
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
+  );
+}
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Users = () => {
+  return (
+    <SUsers>
+      <h2>ユーザー一覧</h2>
+    </SUsers>
+  );
+};
+
+const SUsers = styled.div`
+  text-align: center;
+`;
