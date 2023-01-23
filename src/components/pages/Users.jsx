3f7d7e6b9aa01e38effd9daee8280cfd361c0579
okamura123 <119551754+okamura123@users.noreply.github.com>
2023-01-23 18:36:08 +0900

    qq

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,47 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `${val}kk`,
+    image:
+      "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    email: "123456@example.com",
+    phone: "090-1234-5678",
+    company: {
+      name: "あいう株式会社"
+    },
+    website: "https://google.com"
+  };
+});
 
 export const Users = () => {
   return (
-    <SUsers>
+    <SContainer>
       <h2>ユーザー一覧</h2>
-    </SUsers>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
   );
 };
 
-const SUsers = styled.div`
-  text-align: center;
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: gtid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
